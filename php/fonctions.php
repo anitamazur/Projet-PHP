3f48c921009f98c194e171852c79caf016491634
jenni-h <jenni-h@hotmail.fr>
2012-05-29 18:46:40 +0300

    modifications de la fonction debuthtml au niveau des variables

--- php/fonctions.php
+++ php/fonctions.php
@@ -1,9 +1,9 @@
 <?php 
 function connexion()
 	{
-    $serveurBD = "localhost"; # à modifier pour le serveur de la fac
-    $nomUtilisateur = "root"; # à modifier pour le serveur de la fac
-    $motDePasse = ""; # à modifier pour le serveur de la fac
+    $serveurBD = "localhost"; # à¡­odifier pour le serveur de la fac
+    $nomUtilisateur = "root"; # à¡­odifier pour le serveur de la fac
+    $motDePasse = ""; # à¡­odifier pour le serveur de la fac
     $baseDeDonnees = "essai_annuaire"; # BDD de test / Si BDD ok : changer par "annuaire_defi"
    
     $idConnexion = mysql_connect($serveurBD,
@@ -18,7 +18,7 @@ function connexion()
     #else echo "Echec de connexion a la base"; 
 	} 
 	
-function debuthtml($titre_head,$titre_header='Annuaire M2 DEFI',$titre_contenu)
+function debuthtml($titre_head,$titre_header,$titre_contenu)
 	{
 	echo "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
@@ -38,14 +38,14 @@ function debuthtml($titre_head,$titre_header='Annuaire M2 DEFI',$titre_contenu)
 function finhtml()
 	{
 	echo "</div>
-	<div>Projet de fin d'études des étudiants M2 DEFI</div>
+	<div>Projet de fin d'Ã©tudes des Ã©tudiants M2 DEFI</div>
 	</body>
 	</html>
-	}";
+	";
+	}
 	
 function affichetitre($titre, $n)
 	{
 	echo "<h$n>$titre</h$n>\n" ; 
 	} 
 ?>
- 
