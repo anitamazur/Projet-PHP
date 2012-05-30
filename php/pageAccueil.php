c38c9547b6540441aa727121298587f9e58b6bde
jenni-h <jenni-h@hotmail.fr>
2012-05-31 01:02:04 +0300

    amélioration

--- php/pageAccueil.php
+++ php/pageAccueil.php
@@ -9,105 +9,34 @@ $pass = $_POST['mdp'] ;
 $connexion = connexion() ;
 
 
-
-
-		$champs_vides = array() ;
-		$mauvais_format = array() ;
-														
-		#on teste la valeur des variables transmises par le formulaire
-		if($_SESSION['nom'] == "") {
-			$champs_vides[] = "Nom" ;
-		} else {
-			if(preg_match('/[^A-Za-z \'-]/', $_SESSION['nom'])) {
-				$mauvais_format[] = "Nom" ;
-			}
-		}
-		if($_SESSION['prenom'] == "") {
-			$champs_vides[] = "Prénom" ;
-		} else {
-			if(preg_match('/[^A-Za-z \'-]/', $_SESSION['prenom'])) {
-				$mauvais_format[] = "Prénom" ;
-			}
-		}
-			if($_SESSION['naissance'] == "") {
-			$champs_vides[] = "Date de naissance" ;
-		} else {
-			if(!preg_match('/[^A-Za-z \'-]/', $naissance)) {
-				$mauvais_format[] = "Date de naissance" ;
-			}
-		}
-		
-		#si les tableaux $champs_vides et $mauvais_format sont vides on affiche un message de bienvenue
-		if(count($champs_vides) > 0 || count($mauvais_format) > 0 ) {
-			#test des champs vides
-			if(count($champs_vides) == 0 ) {
-				$message = "" ; 
-			} else {
-				$n = count($champs_vides) ;
-				if($n > 1) {
-					$message = "<p class=\"erreur\">Vous devez remplir les champs : " ; 
-				} else {
-					$message = "<p class=\"erreur\">Vous devez remplir le champ " ;
-				}
-				for($i=0; $i<$n; $i++) {
-					$champ = $champs_vides[$i] ; 
-					if($i == 0) {
-						$message .= "$champ</p>" ;
-					} elseif($i == $n-1) {
-						$message .= " et $champ</p>" ; 
-					} else {
-						$message .= ",  $champ</p>" ;
-					}
-				}
-			}
-
-			#test des champs mal remplis
-			if(count($mauvais_format) == 0 ) {
-				$fin_message = "" ; 
-			} else {
-				$n = count($mauvais_format);
-				if($n > 1) {
-					$message .= "<p class=\"erreur\">Les champs " ;
-					$fin_message = " n'ont pas été saisis correctement.</p>" ;
-				} else {
-					$message .= "<p class=\"erreur\">Le champ " ;
-					$fin_message = " n'a pas été saisi correctement.</p>" ;
-				}
-				for($i=0; $i<$n; $i++){
-					$champ = $mauvais_format[$i] ; 
-					if($i == 0) {
-						$message .= "$champ" ;
-					} elseif($i == $n-1) {
-						$message .= " et $champ" ;
-					} else {
-						$message .= ",  $champ" ;
-					}
-				}
-			}
-			$message .= $fin_message ;
-		}
-	}
-
-$req = "select * from utilisateur where nom='$nom' AND prenom='$prenom' AND naisance='$naissance' AND pass='$pass'" ;
+$req = "select * from utilisateur where nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
 $res = mysql_query($req) ;
 
+if(mysql_num_rows($res)>0) {
 $ligne=mysql_fetch_object($res) ;
-	$id = $ligne->id ;
-	$nom = $ligne->nom ;
-	$prenom = $ligne->prenom ;
+$id = $ligne->id ;
+$nom = $ligne->nom ;
+$prenom = $ligne->prenom ;}
 
 
-$req_b = "select role,id_role,u.id from role AS r,role_utilisateur AS ru, utilisateur AS u where r.id=ru.id_role and u.id=ru.id_utilisateur and id_utilisateur='$id'" ; 
 
+$req_b = "select role,id_role,u.id from role AS r,roles_utilisateur AS ru, utilisateur AS u where r.id=ru.id_role and u.id=ru.id_utilisateur and u.id_utilisateur='$id'" ; 
 $res_b = mysql_query($req_b) ;
+
+if(mysql_num_rows($res_b)>0) {
 $ligne=mysql_fetch_object($res_b) ;
-	$id_utilisateur = $ligne->id_utilisateur;
 	$id_role = $ligne->id_role;
-	$role = $ligne->role;
+	$role = $ligne->role;}
 
-$req_c = "select statut,id_statut, u.id from statut AS s, statut_utilisateur AS su, utilisateur AS u where s.id=su.id_statut and u.id=su.id_utilisateur and id_utilisateur='$id'" ;
+$req_c = "select statut,id_statut, u.id from statut AS s, statut_ancien_etudiant AS sa, utilisateur AS u where s.id=sa.id_statut and u.id=sa.id_utilisateur and id_utilisateur='$id'" ;
+$res_c = mysql_query($req_c) ;
 
-if(mysql_num_rows($res) > 0)
+if(mysql_num_rows($res_c)>0) {
+$ligne=mysql_fetch_object($res_c) ;
+	$id_statut = $ligne->id_statut;
+	$statut = $ligne->statut; }
+
+if(mysql_num_rows($res)>0)
 	{
 	session_start() ;
 	$ligne=mysql_fetch_object($res) ;
@@ -168,10 +97,7 @@ if(mysql_num_rows($res) > 0)
   
 else {
 	echo "<p class=\"erreur\">Erreur à l'identification</p>" ;
-	}
-	if (isset($message)) {
-		echo $message;
-		echo "<p>Retournez à la <a href=\"connexion.php\">page de connexion</a>.</p>";
+	echo "<p>Retournez à la <a href=\"connexion.php\">page de connexion</a>.</p>";
 	}
     
 mysql_close() ;
