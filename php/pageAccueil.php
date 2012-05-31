25c629b148fd060ce55b185ef937d44eb76ee8fa
jenni-h <jenni-h@hotmail.fr>
2012-05-31 10:34:56 +0300

    modification dans la page d'accueil avec une simple requête :)

--- php/pageAccueil.php
+++ php/pageAccueil.php
@@ -9,42 +9,30 @@ $pass = $_POST['mdp'] ;
 $connexion = connexion() ;
 
 
-$req = "select * from utilisateur where nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
+$req = "SELECT * 
+	FROM utilisateur AS u, role AS r, roles_utilisateur AS ru, statut AS s, statut_ancien_etudiant AS sa 
+	WHERE u.id = ru.id_utilisateur
+	AND u.id = sa.id_utilisateur
+	AND r.id = ru.id_role
+	AND s.id = sa.id_statut
+	AND nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
 $res = mysql_query($req) ;
 
-if(mysql_num_rows($res)>0) {
-$ligne=mysql_fetch_object($res) ;
-$id = $ligne->id ;
-$nom = $ligne->nom ;
-$prenom = $ligne->prenom ;}
-
-
-
-$req_b = "select role,id_role,u.id from role AS r,roles_utilisateur AS ru, utilisateur AS u where r.id=ru.id_role and u.id=ru.id_utilisateur and u.id_utilisateur='$id'" ; 
-$res_b = mysql_query($req_b) ;
-
-if(mysql_num_rows($res_b)>0) {
-$ligne=mysql_fetch_object($res_b) ;
-	$id_role = $ligne->id_role;
-	$role = $ligne->role;}
-
-$req_c = "select statut,id_statut, u.id from statut AS s, statut_ancien_etudiant AS sa, utilisateur AS u where s.id=sa.id_statut and u.id=sa.id_utilisateur and id_utilisateur='$id'" ;
-$res_c = mysql_query($req_c) ;
-
-if(mysql_num_rows($res_c)>0) {
-$ligne=mysql_fetch_object($res_c) ;
-	$id_statut = $ligne->id_statut;
-	$statut = $ligne->statut; }
 
 if(mysql_num_rows($res)>0)
 	{
 	session_start() ;
 	$ligne=mysql_fetch_object($res) ;
 	$_SESSION['nom'] = $ligne->nom ;
-	$_SESSION['prenom'] = $ligne->prenom ; 
+	$_SESSION['prenom'] = $ligne->prenom ; 	
+	$_SESSION['id_role'] = $ligne->id_role ;
+	$_SESSION['role'] = $ligne->role ;
+	$_SESSION['id_statut'] = $ligne->id_statut;
+	$_SESSION['statut'] = $ligne->statut;
 
 	$prenom = ucfirst(strtolower($_SESSION['prenom']));
 	$nom = ucfirst(strtolower($_SESSION['nom']));
+	
 
 	debutpagehtml("Annuaire M2 DEFI - Accueil","Annuaire M2 DEFI", "Accueil") ;
 	
