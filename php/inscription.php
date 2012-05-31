6e3c24cc9b151abbcf264122c2bc54fdaf812f9e
jenni-h <jenni-h@hotmail.fr>
2012-06-01 01:16:27 +0300

    mise en place des fonctions pour debuthtml et finhtml

--- php/inscription.php
+++ php/inscription.php
@@ -4,23 +4,10 @@
 	$connexion = connexion();
 	// On démarre la session
 	session_start();
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-		<title>Annuaire M2 DEFI - Inscription</title>
-		<link rel="stylesheet" type="text/css" media="screen" href="defi.css" />
-	</head>
-	<body>
-		<?php
+		
+		debuthtml ("Annuaire M2 DEFI - Inscription","Annuaire M2 DEFI","Inscription à l'annuaire");
 			afficheMenu();
 		?>
-		<div id="header">
-			<h1>Annuaire M2 DEFI</h1>
-		</div>
-		<div id="contenu">
-			<h2>Inscription au site</h2>
 			<p>* : champs obligatoires</p>
 			<form id="inscription" action="inscription.php" method="post">
 				<fieldset>
@@ -110,9 +97,9 @@
 							$message_ajout = "<p class=\"erreur\">L'adresse E-Mail à ajouter n'a pas le bon format.</p>";
 						}
 					}
-				}
+			
+			}
+	finhtml();
+			mysql_close ();
 			?>
-		</div>
-	</body>
-</html>
 		
