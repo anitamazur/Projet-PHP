7fd3a7c78a68e24cea12a24e369b41e74890726b
anita test <anita.mazur1@gmail.com>
2012-06-01 16:07:29 +0200

    ajout d'un tableau pour afficher sa promo

---
+++ php/mapromo.php
@@ -0,0 +1,101 @@
+<?php
+
+session_start() ;
+$nom = $_SESSION['nom'] ;
+$prenom = $_SESSION['prenom'] ;
+
+require("fonctions.php") ;
+$connexion = connexion() ;
+
+debuthtml("Annuaire M2 DEFI - Ma promo","Annuaire M2 DEFI", "Ma promotion") ;
+
+		echo"<table border=\"1px\">
+			<th>Promotion</th>
+			<tr>
+			<td>Nom Prénom</td>
+			<td>Contact</td>
+			<td>Statut</td>
+			<td>Situation actuelle</td>
+			<td>Date d'inscription</td>
+			<td>Date de mise à jour du profil</td>
+			</tr>";
+
+$req_anneepromo = "SELECT * from utilisateurwhere nom='$nom' AND prenom='$prenom' " ;
+
+$res_anneepromo = mysql_query($req_anneepromo) ;
+
+if(mysql_num_rows($res_anneepromo) > 0)
+{
+$ligne=mysql_fetch_object($res) ;
+$_SESSION['nom'] = $ligne->nom ;
+$_SESSION['prenom'] = $ligne->prenom ;
+$annee_promo = $ligne->annee_promo ;
+
+$req_promo = "SELECT * 
+	FROM utilisateur AS u, role AS r, roles_utilisateur AS ru, statut AS s, statut_ancien_etudiant AS sa, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp,etudes AS et, etudes_utilisateur AS etu, etablissement AS eta, etablissement_utilisateur AS etau
+	WHERE u.id = ru.id_utilisateur
+	AND u.id = sa.id_utilisateur
+	AND r.id = ru.id_role
+	AND s.id = sa.id_statut
+	AND u.id = pu.id_utilisateur
+	AND u.id = eu.id_utilisateur
+	AND u.id = etu.id_utilisateur
+	AND u.id = etau.id_utilisateur
+	AND vi.id = vp.id_ville
+	AND pa.id = vp.id_pays
+	AND e.id = eu.id_entreprise
+	AND e.id = pde.id_entreprise
+	AND e.id = ev.id_entreprise
+	AND vi.id = ev.id_entreprise
+	AND et.id = etu.id_etudes
+	AND eta.id = etau.id_etablissement
+	AND v.id = etav.id_ville
+	AND vi.id = vp.id_ville
+	AND pa.id = vp.id_pays
+	AND annee_promo='$annee_promo' ORDER BY nom";
+
+$res_promo = mysql_query($req_promo) ;
+
+while ($ligne = mysql_fetch_object($res_promo)) {
+						echo"<th>".$ligne->annee_promo."</th>";
+						echo "<tr>"
+						echo "<td>".$ligne->nom."".$ligne->prenom."</td>";
+						echo "<td>".$ligne->mail."</td>";
+						echo "<td>".$ligne->role."</td>";
+						if ($role =1)
+							{
+							echo "<td>".$ligne->statut."</td>";
+							if ($statut=1)
+								{
+								echo "<td>".$ligne->diplome_etudes."</br>
+								".$ligne->nom_etablissement" ".$ligne->siteweb_etabllissement." ".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
+								}
+							elseif ($statut=2)
+								{
+								echo "<td>".$ligne->nom_poste."</br>
+								".$ligne->nom_entreprise" ".$ligne->siteweb_entreprise." ".$ligne->secteur_entreprise." ".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
+								}
+							elseif ($statut=3)
+								{
+								echo "<td>".$ligne->statut."</td>";
+								}
+							}
+						if ($role = 2)
+							{
+							echo "<td>".$ligne->diplome_etudes."</br>
+								".$ligne->nom_etablissement" ".$ligne->siteweb_etabllissement." ".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
+							}
+						
+						echo "<td>".$ligne->date_inscription."<td>"
+						echo "<td>".$ligne->date_maj_profil."<td>"
+						echo "</tr>";
+					}
+
+	  echo "</table>";	
+
+	  echo "<p>Si vous rencontrez des problémes n'hésitez pas à <a href=\"mailto:admin@annuairedefi.u-paris10.fr\">contacter l'administrateur</a></p>";
+	  
+	  finhtml();
+	  
+	  mysql_close();
+?>
