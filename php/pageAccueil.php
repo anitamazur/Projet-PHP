e2258ce40ca7b34bb16c5aec63b73ac117b24465
Anita Mazur <aanitah@gmail.com>
2012-06-02 22:36:34 +0200

    ajout de la gestion de l'affichage d'un autre contenu sur la page d'accueil selon le rôle et de dates d'inscription et de mise à jour

--- php/pageAccueil.php
+++ php/pageAccueil.php
@@ -5,6 +5,7 @@ $nom = $_POST['nom'] ;
 $prenom = $_POST['prenom'] ;
 $naissance = $_POST['naissance'] ;
 $mail = $_POST['mail'] ;
+//code pour decrypter le mot de passe qui a été crypté à l'inscription.
 $salt = "ashrihgbjnbfj";
 $pass = crypt($_POST['mdp'], $salt);
 
@@ -13,88 +14,112 @@ $_SESSION['nom'] = $nom;
 $_SESSION['prenom'] = $prenom;
 $_SESSION['naissance'] = $naissance;
 $_SESSION['pass'] = $pass;
+$_SESSION['mail'] = $mail;
 
 $connexion = connexion() ; 
 
-
-
-if(connexionUtilisateurReussie())
-
-{
-	
-
-	$req = "SELECT * 
-		FROM utilisateur AS u, role AS r, roles_utilisateur AS ru, statut AS s, statut_ancien_etudiant AS sa 
-		WHERE u.id = ru.id_utilisateur
-		AND u.id = sa.id_utilisateur
-		AND r.id = ru.id_role
-		AND s.id = sa.id_statut
-		AND nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
-	$res = mysql_query($req) ;
-	$ligne=mysql_fetch_object($res) ;
-	$nom = $ligne->nom ;
-	$prenom = $ligne->prenom ; 	
-	$id_role = $ligne->id_role ;
-	$id_statut = $ligne->id_statut ;
-	$role = $ligne->nom_role ;
-	$statut = $ligne->nom_statut ; 
-	$annee_promo = $ligne->annee_promo ;
-	$mail = $ligne->mail ;
-	$pass = $ligne->pass ;
-	$prenom = ucfirst(strtolower($prenom));
-	$nom = ucfirst(strtolower($nom));
-	$date_inscription = date($ligne->date_inscription) ;
-	$date_inscription_plus_un_an = strtotime(date("Y-m-d", strtotime($date_inscription)) . " +12 month");
-	$date_inscription_plus_un_an = date("Y-m-d",$date_inscription_plus_un_an);
-	$date_maj_profil = date($ligne->date_maj_profil) ;
-
+//affichage d'une page d'accueil personnalisée selon le rôle
+if(connexionUtilisateurReussie()) {
 	debuthtml("Annuaire M2 DEFI - Accueil","Annuaire M2 DEFI", "Accueil") ;
-	 
-	
-		if ($id_role == 1)
-		{ 			
-			affichetitre("Vos informations personnelles :","3") ;
-			echo "<p>$nom $prenom</p>
-			<p><strong>$role</strong> : $statut</p>
-			<p><strong>Année de la promotion</strong> : $annee_promo</p>
-			<p><strong>Adresse mail</strong> : $mail </p><br/>";	
-			
-			if ($id_statut == 1) {
-				echo "<p>Veuillez remplir votre profil en cliquant sur l'onglet <a href=\"Gestiondeprofil.php\">Gestion de mon profil</a>.</p>";
-			}
-			
-			elseif (($id_statut != 1) && ($date_maj_profil > $date_inscription_plus_un_an)) {
-				echo "<p>Veuillez mettre à jour votre profil en cliquant sur l'onglet <a href=\"Gestiondeprofil.php\">Gestion de mon profil</a>.</p>";
-			} 
-			
-			
+	affichetitre("Vos informations personnelles :","3") ;
+	$id_utilisateur = getID($nom, $prenom, $mail);
+	$id_role = role($id_utilisateur);
+	$id_statut = statut($id_utilisateur);
+	if ($id_role == 1) {
+		$req = "SELECT * 
+			FROM utilisateur AS u, role AS r, roles_utilisateur AS ru, statut AS s, statut_ancien_etudiant AS sa 
+			WHERE u.id = ru.id_utilisateur
+			AND u.id = sa.id_utilisateur
+			AND r.id = ru.id_role
+			AND s.id = sa.id_statut
+			AND nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
+		$res = mysql_query($req) ;
+		$ligne=mysql_fetch_object($res) ;
+		$nom = ucfirst(strtolower($ligne->nom)) ;
+		$prenom = ucfirst(strtolower($ligne->prenom)) ;
+		$id_role = $ligne->id_role ;
+		$id_statut = $ligne->id_statut ;
+		$role = $ligne->nom_role ;
+		$statut = $ligne->nom_statut ; 
+		$annee_promo = $ligne->annee_promo ;
+		$mail = $ligne->mail ;
+		$date_inscription = date($ligne->date_inscription) ;
+		$date_inscription_plus_un_an = strtotime(date("Y-m-d", strtotime(date($ligne->date_inscription))) . " +12 month");
+		$date_inscription_plus_un_an = date("Y-m-d",$date_inscription_plus_un_an);
+		$date_maj_profil = date($ligne->date_maj_profil) ;
 
-			
-			
-		elseif ($id_role == 2)
-		{
-			affichetitre("2","vos informations personnelles :") ;
-			echo "<p>$nom $prenom</p>
-			<p><strong>$role</strong> : $statut</p>
-			<p><strong>Année de la promotion</strong> : $annee_promo</p>
-			<p><strong>Adresse mail</strong> : $mail </p><br/>";
+		echo "<p>$nom $prenom</p>
+		<p><strong>$role</strong> : $statut</p>
+		<p><strong>Année de la promotion</strong> : $annee_promo</p>
+		<p><strong>Adresse mail</strong> : $mail </p>
+		<p><strong>Date d'inscription</strong> : $date_inscription </p>
+		<p><strong>Date de mise à jour du profil</strong> : $date_maj_profil </p>
+		<br/>";	
+		//Si l'utilisateur a le statut "profil à remplir" (id1) on affiche un message qu'il doit remplir son profil.
+		if ($id_statut == 1) {
+			echo "<p>Veuillez remplir votre profil en cliquant sur l'onglet <a href=\"Gestiondeprofil.php\">Gestion de mon profil</a>.</p>";
 		}
-		
-		
-		elseif ($id_role == 3 or $id_role == 4)
-		{
-			affichetitre("2","Vos informations personnelles :") ;
-			echo "<p>$nom $prenom</p>
-			<p><strong>$role</strong> : $statut</p>
-			<p><strong>Adresse mail</strong> : $mail </p><br/>";
-		}
+		//Calcul our savoir si les informations sont obsolètes ou non
+		elseif (($id_statut != 1) && ($date_maj_profil > $date_inscription_plus_un_an)) {
+			echo "<p>Veuillez mettre à jour votre profil en cliquant sur l'onglet <a href=\"Gestiondeprofil.php\">Gestion de mon profil</a>.</p>";
+		} 
+	}
+	elseif ($id_role == 2)
+	{
+		$req = "SELECT * 
+			FROM utilisateur AS u, role AS r, roles_utilisateur AS ru 
+			WHERE u.id = ru.id_utilisateur
+			AND r.id = ru.id_role
+			AND nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
+		$res = mysql_query($req) ;
+		$ligne=mysql_fetch_object($res) ;
+		$nom = ucfirst(strtolower($ligne->nom)) ;
+		$prenom = ucfirst(strtolower($ligne->prenom)) ; 	
+		$role = $ligne->nom_role ;
+		$annee_promo = $ligne->annee_promo ;
+		$mail = $ligne->mail ;
+		$date_inscription = date($ligne->date_inscription) ;
+		$date_maj_profil = date($ligne->date_maj_profil) ;
+		affichetitre("Vos informations personnelles :","3") ;
+		echo "<p>$nom $prenom</p>
+		<p><strong>$role</strong></p>
+		<p><strong>Année de la promotion</strong> : $annee_promo</p>
+		<p><strong>Adresse mail</strong> : $mail </p>
+		<p><strong>Date d'inscription</strong> : $date_inscription </p>
+		<p><strong>Date de mise à jour du profil</strong> : $date_maj_profil </p>
+		<br/>";
+	}
+	
+	
+	elseif ($id_role == 3 or $id_role == 4)
+	{
+		$req = "SELECT * 
+			FROM utilisateur AS u, role AS r, roles_utilisateur AS ru 
+			WHERE u.id = ru.id_utilisateur
+			AND r.id = ru.id_role
+			AND nom='$nom' AND prenom='$prenom' AND naissance='$naissance' AND pass='$pass'" ;
+		$res = mysql_query($req) ;
+		$ligne=mysql_fetch_object($res) ;
+		$nom = ucfirst(strtolower($ligne->nom)) ;
+		$prenom = ucfirst(strtolower($ligne->prenom)) ;
+		$role = $ligne->nom_role ;
+		$mail = $ligne->mail ;
+		$date_inscription = date($ligne->date_inscription) ;
+		$date_maj_profil = date($ligne->date_maj_profil) ;
+		affichetitre("Vos informations personnelles :","3") ;
+		echo "<p>$nom $prenom</p>
+		<p><strong>$role</strong> : $statut</p>
+		<p><strong>Adresse mail</strong> : $mail </p>
+		<p><strong>Date d'inscription</strong> : $date_inscription </p>
+		<p><strong>Date de mise à jour du profil</strong> : $date_maj_profil </p>
+		<br/>";
+	}
 
 	
 	echo "<p>Si vous rencontrez des problémes n'hésitez pas à <a href=\"mailto:admin@annuairedefi.u-paris10.fr\">contacter l'administrateur</a></p>";
 	afficheMenu($id_role);
 	finhtml() ;
 	}
-}
   
 else {
 	echo "<p class=\"erreur\">Erreur à l'identification</p>" ;
