12b8a10b522164135c96dde0cbfb089c463caf2e
jenni-h <jenni-h@hotmail.fr>
2012-06-08 00:50:18 +0300

    modifications effectuées en espérant que ça fonctionne ^^

--- php/lespromos.php
+++ php/lespromos.php
@@ -12,15 +12,15 @@ debuthtml("Annuaire M2 DEFI - Ma promo","Annuaire M2 DEFI", "Ma promotion") ;
 		echo"<table border=\"1px\">
 			<th>Promotion</th>
 			<tr>
-			<td>Nom Prénom</td>
+			<td>Nom Prï¿½nom</td>
 			<td>Contact</td>
 			<td>Statut</td>
 			<td>Situation actuelle</td>
 			<td>Date d'inscription</td>
-			<td>Date de mise à jour du profil</td>
+			<td>Date de mise ï¿½ jour du profil</td>
 			</tr>";
 
-$req_promo = "SELECT * 
+$res_promo =mysql_query( "SELECT * 
 	FROM utilisateur AS u, role AS r, roles_utilisateur AS ru, statut AS s, statut_ancien_etudiant AS sa, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp,etudes AS et, etudes_utilisateur AS etu, etablissement AS eta, etablissement_utilisateur AS etau, etablissement_ville AS etav
 	WHERE u.id = ru.id_utilisateur
 	AND u.id = sa.id_utilisateur
@@ -42,9 +42,8 @@ $req_promo = "SELECT *
 	AND v.id = etav.id_ville
 	AND vi.id = vp.id_ville
 	AND pa.id = vp.id_pays
-	ORDER BY nom";
+	ORDER BY nom");
 
-$res_promo = mysql_query($req_promo) ;
 
 while ($ligne = mysql_fetch_object($res_promo)) {
 						echo"<th>".$ligne->annee_promo."</th>";
@@ -52,22 +51,23 @@ while ($ligne = mysql_fetch_object($res_promo)) {
 						echo "<td>".$ligne->nom."".$ligne->prenom."</td>";
 						echo "<td>".$ligne->mail."</td>";
 						echo "<td>".$ligne->role."</td>";
-						if ($role =1)
+						if ($role ==1)
 							{
 							echo "<td>".$ligne->statut."</td>";
-							if ($statut=1)
-								{
-								echo "<td>".$ligne->diplome_etudes."</br>
-								".$ligne->nom_etablissement." ".$ligne->siteweb_etabllissement."<br/>
-								".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
-								}
-							elseif ($statut=2)
+							if ($statut==2)
 								{
 								echo "<td>".$ligne->nom_poste."</br>
 								".$ligne->nom_entreprise." ".$ligne->siteweb_entreprise." ".$ligne->secteur_entreprise." <br/>
 								".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
 								}
-							elseif ($statut=3)
+								
+							elseif ($statut==3)
+								{
+								echo "<td>".$ligne->diplome_etudes."</br>
+								".$ligne->nom_etablissement." ".$ligne->siteweb_etabllissement."<br/>
+								".$ligne->nom_ville." ".$ligne->cp." ".$ligne->nom_pays." </td>";
+								}
+							elseif ($statut==1 or $statut==4)
 								{
 								echo "<td>".$ligne->statut."</td>";
 								}
@@ -80,7 +80,7 @@ while ($ligne = mysql_fetch_object($res_promo)) {
 
 	  echo "</table>";	
 
-	  echo "<p>Si vous rencontrez des problémes n'hésitez pas à <a href=\"mailto:admin@annuairedefi.u-paris10.fr\">contacter l'administrateur</a></p>";
+	  echo "<p>Si vous rencontrez des problï¿½mes n'hï¿½sitez pas ï¿½ <a href=\"mailto:admin@annuairedefi.u-paris10.fr\">contacter l'administrateur</a></p>";
 	  
 	  finhtml();
 	  
