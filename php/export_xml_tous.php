3751434d8f7a83f6634ec2b1cc69549419925293
jenni-h <jenni-h@hotmail.fr>
2012-06-11 17:46:39 +0300

    mise en commentaire de $_SESSION['pass']

--- php/export_xml_tous.php
+++ php/export_xml_tous.php
@@ -4,8 +4,8 @@ session_start() ;
 $nom = $_SESSION['nom'];
 $prenom = $_SESSION['prenom'];
 $naissance = $_SESSION['naissance'];
-$salt = "ashrihgbjnbfj";
-$pass = crypt($_SESSION['pass'], $salt);
+#$salt = "ashrihgbjnbfj";
+#$pass = crypt($_SESSION['pass'], $salt);
 $mail = $_SESSION['mail'] ;
 
 require_once("fonctions.php") ;
