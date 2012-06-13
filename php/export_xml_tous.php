1bc1e56b80999474c53699667d94b7c50f00eecc
jenni-h <jenni-h@hotmail.fr>
2012-06-13 20:46:30 +0300

    à tester mais je crois pb d'encodage avec les accents :/

--- php/export_xml_tous.php
+++ php/export_xml_tous.php
@@ -4,11 +4,7 @@ $nom = $_SESSION['nom'] ;
 $prenom = $_SESSION['prenom'] ;
 $naissance = $_SESSION['naissance'] ;
 $mail = $_SESSION['mail'] ;
-$id_utilisateur = $_SESSION['id_utilisateur'] = getID($nom, $prenom, $mail);
-$id_role = $_SESSION['id_role'] = role($id_utilisateur);
-$id_statut = $_SESSION['id_statut'] = statut($id_utilisateur);
-$id_role = $_SESSION['id_role'] = role($id_utilisateur);
-$id_statut = $_SESSION['id_statut'] = statut($id_utilisateur);
+
 
 
 require_once("fonctions.php") ;
@@ -26,7 +22,7 @@ $req = "SELECT *
 
 $res = mysql_query($req) ;
 
-$xml = '<?xml version="1.0" encoding="utf-8"?>'.'<profil>';
+$xml = '<?xml version="1.0"?>'.'<profil>';
 		while ($row = mysql_fetch_array($res)) {
 			$xml .= '<personne>';
 			$xml .= '<nom>'.$row['nom'].'</nom>';
@@ -95,7 +91,7 @@ $xml = '<?xml version="1.0" encoding="utf-8"?>'.'<profil>';
 			}
 		}
 		
-		elseif ($id_role == 1 &&  ( $id_statut == 1 or $id_statut == 4)) {
+		elseif ($row['id_role'] == 1 &&  ( $row['id_statut'] == 1 or $row['id_statut'] == 4)) {
 			$xml .= '<nom_statut>'.$row['nom_statut'].'</nom_statut>';
 		
 		} 
@@ -110,7 +106,7 @@ $xml = '<?xml version="1.0" encoding="utf-8"?>'.'<profil>';
 		fputs($fp, $xml);
 		fclose($fp);
 		
-		echo 'Export XML effectu� !<br><a href="profils.xml">Voir le fichier</a>';
+		echo 'Export XML effectu? !<br><a href="profils.xml">Voir le fichier</a>';
 	
 		finhtml();
 		mysql_close();
