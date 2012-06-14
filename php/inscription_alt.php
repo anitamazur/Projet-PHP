a983e5600a6344780db372e5b13c43ef242e0ccd
anita test <anita.mazur1@gmail.com>
2012-06-14 15:27:11 +0200

    correction de l'indentation

--- php/inscription_alt.php
+++ php/inscription_alt.php
@@ -5,94 +5,82 @@
     require_once("fonctions.php");
     $connexion = connexion();
     $message_ajout = "";
-	//On vérifie si l'utilisateur a cliqué sur le bouton "Valider", si oui on crée une requete SQL permettant d'ajouter dans la base de données les données rentrées par l'utilisateur. Celle-ci ne sera executée seulement si les champs à remplir pour rentrer ne sont ni vides, ni au mauvais format. 
-	if(isset($_POST['valider'])) {
-		$mail = stripslashes($_POST['mail']);
-		$nom = stripslashes($_POST['nom']);
-		$nomPatro = stripslashes($_POST['nomPatro']);
-		$prenom = stripslashes($_POST['prenom']);
-		$naissance = stripslashes($_POST['naissance']);
-		$anneePromo = stripslashes($_POST['anneePromo']);
-		$mdp = stripslashes($_POST['mdp']);
-		$mdpRepete = stripslashes($_POST['repeat_mdp']);
-		$role = stripslashes($_POST['role']);
-		if ($mdp != $mdpRepete) { 
-			$message_ajout = "<p class=\"erreur\">Les 2 mots de passe sont différents.</p>";
-		}
-		if($mail == "") {
-			$message_ajout = "<p class=\"erreur\">Le champ 'E-Mail à ajouter' est vide.</p>";
-		} else {
-			// On vérifie si l'adresse E-mail rentrée par l'utilisateur est au bon format
-			$mail_ok = VerifierAdresseMail($mail);
-			if($mail_ok == true) {
-				$reqInscription = "INSERT INTO utilisateur (mail, mail_pro, pass, cle_activation, compte_active, nom, nom_patronymique, prenom, naissance, annee_promo, date_inscription, date_maj_profil) VALUES ('$mail','', ENCRYPT('$mdp', 'ashrihgbjnbfj'), '', '', '$nom', '$nomPatro', '$prenom', '$naissance', '$anneePromo', now(), now())" ;
-				$resAjout = mysql_query($reqInscription) ;
-				$id = mysql_insert_id();
-				if($resAjout <> FALSE) {
-					$message_ajout = "<p class=\"succes\">Profil enregistré dans la base de données.</p> <p>Vous pouvez vous connecter désormais en cliquant sur le point de menu <a href=\"connexion.php\">Connexion</a></p>" ;
-					$relInscription = "INSERT INTO roles_utilisateur (id_utilisateur, id_role) VALUES ('$id','$role')" ;
-					$relAjout = mysql_query($relInscription) ;
-					if ($role == 1) {
-						$statut = 1;
-						$statutInscription = "INSERT INTO statut_ancien_etudiant (id_utilisateur, id_statut) VALUES ('$id','$statut')" ;
-						$statutAjout = mysql_query($statutInscription) ;
-						
-						$affichage_mail = stripslashes($_POST['affichage_mail']);
-						$affichage_nom = stripslashes($_POST['affichage_nom']);
-						$affichage_nomPatro = stripslashes($_POST['affichage_nomPatro']);
-						$affichage_prenom = stripslashes($_POST['affichage_prenom']);
-			
-								if ($affichage_nom == 1)
-								{
-								$res_an=mysql_query("INSERT INTO utilisateur (nom_niveau) VALUES ('prive')");
-								}
-								else
-								{
-								$res_an=mysql_query("INSERT INTO utilisateur (nom_niveau) VALUES ('public')");
-								} 
-								
-								if ($affichage_nomPatro == 1)
-								{
-								$res_anp=mysql_query("INSERT INTO utilisateur (nomPatro_niveau) VALUES ('prive')");
-								}
-								else
-								{
-								$res_anp=mysql_query("INSERT INTO utilisateur (nomPatro_niveau) VALUES ('public')");
-								}
-								
-								if ($affichage_prenom == 1)
-								{
-								$res_p=mysql_query("INSERT INTO utilisateur (prenom_niveau) VALUES ('prive')");
-								}
-								else
-								{
-								$res_p=mysql_query("INSERT INTO utilisateur (prenom_niveau) VALUES ('public')");
-								}
-								
-								if ($affichage_mail == 1)
-								{
-								$res_m=mysql_query("INSERT INTO utilisateur (mail_niveau) VALUES ('prive')");
-								}
-								else
-								{
-								$res_m=mysql_query("INSERT INTO utilisateur (mail_niveau) VALUES ('public')");
-								}
-								
-						}
-						if ($role == 2) {
-						$confidentialite_req = "UPDATE utilisateur SET nom_niveau = '', prenom_niveau = '', mail_niveau = '', mailPro_niveau = '' WHERE utilisateur.id = $id;" ;
-						$confidentialite_Ajout = mysql_query($confidentialite_req) ;
-					}			
-					
-				} else {
-					$message_ajout = "<p class=\"erreur\">Erreur lors de l'enregistrement.</p>" ;
-				}
-			} elseif ($mail_ok == false) {
-				$message_ajout = "<p class=\"erreur\">L'adresse E-Mail à ajouter n'a pas le bon format.</p>";
-			}
-		}
+    //On vérifie si l'utilisateur a cliqué sur le bouton "Valider", si oui on crée une requete SQL permettant d'ajouter dans la base de données les données rentrées par l'utilisateur. Celle-ci ne sera executée seulement si les champs à remplir pour rentrer ne sont ni vides, ni au mauvais format. 
+    if(isset($_POST['valider'])) {
+        $mail = stripslashes($_POST['mail']);
+        $nom = stripslashes($_POST['nom']);
+        $nomPatro = stripslashes($_POST['nomPatro']);
+        $prenom = stripslashes($_POST['prenom']);
+        $naissance = stripslashes($_POST['naissance']);
+        $anneePromo = stripslashes($_POST['anneePromo']);
+        $mdp = stripslashes($_POST['mdp']);
+        $mdpRepete = stripslashes($_POST['repeat_mdp']);
+        $role = stripslashes($_POST['role']);
+        if ($mdp != $mdpRepete) { 
+            $message_ajout = "<p class=\"erreur\">Les 2 mots de passe sont différents.</p>";
+        }
+        if($mail == "") {
+            $message_ajout = "<p class=\"erreur\">Le champ 'E-Mail à ajouter' est vide.</p>";
+        } else {
+            // On vérifie si l'adresse E-mail rentrée par l'utilisateur est au bon format
+            $mail_ok = VerifierAdresseMail($mail);
+            if($mail_ok == true) {
+                $reqInscription = "INSERT INTO utilisateur (mail, mail_pro, pass, cle_activation, compte_active, nom, nom_patronymique, prenom, naissance, annee_promo, date_inscription, date_maj_profil) VALUES ('$mail','', ENCRYPT('$mdp', 'ashrihgbjnbfj'), '', '', '$nom', '$nomPatro', '$prenom', '$naissance', '$anneePromo', now(), now())" ;
+                $resAjout = mysql_query($reqInscription) ;
+                $id = mysql_insert_id();
+                if($resAjout) {
+                    $message_ajout = "<p class=\"succes\">Profil enregistré dans la base de données.</p> <p>Vous pouvez vous connecter désormais en cliquant sur le point de menu <a href=\"connexion.php\">Connexion</a></p>" ;
+                    $relInscription = "INSERT INTO roles_utilisateur (id_utilisateur, id_role) VALUES ('$id','$role')" ;
+                    $relAjout = mysql_query($relInscription) ;
+                    if ($role == 1) {
+                        $statut = 1;
+                        $statutInscription = "INSERT INTO statut_ancien_etudiant (id_utilisateur, id_statut) VALUES ('$id','$statut')" ;
+                        $statutAjout = mysql_query($statutInscription) ;
+                        
+                        $affichage_mail = stripslashes($_POST['affichage_mail']);
+                        $affichage_nom = stripslashes($_POST['affichage_nom']);
+                        $affichage_nomPatro = stripslashes($_POST['affichage_nomPatro']);
+                        $affichage_prenom = stripslashes($_POST['affichage_prenom']);
+            
+                                if ($affichage_nom == 1) {
+                                    $res_an=mysql_query("INSERT INTO utilisateur (nom_niveau) VALUES ('prive')");
+                                } else {
+                                    $res_an=mysql_query("INSERT INTO utilisateur (nom_niveau) VALUES ('public')");
+                                } 
+                                
+                                if ($affichage_nomPatro == 1) {
+                                    $res_anp=mysql_query("INSERT INTO utilisateur (nomPatro_niveau) VALUES ('prive')");
+                                } else {
+                                    $res_anp=mysql_query("INSERT INTO utilisateur (nomPatro_niveau) VALUES ('public')");
+                                }
+                                
+                                if ($affichage_prenom == 1) {
+                                    $res_p=mysql_query("INSERT INTO utilisateur (prenom_niveau) VALUES ('prive')");
+                                } else {
+                                    $res_p=mysql_query("INSERT INTO utilisateur (prenom_niveau) VALUES ('public')");
+                                }
+                                
+                                if ($affichage_mail == 1) {
+                                    $res_m=mysql_query("INSERT INTO utilisateur (mail_niveau) VALUES ('prive')");
+                                } else {
+                                    $res_m=mysql_query("INSERT INTO utilisateur (mail_niveau) VALUES ('public')");
+                                }
+                                
+                        }
+                        if ($role == 2) {
+                        $confidentialite_req = "UPDATE utilisateur SET nom_niveau = '', prenom_niveau = '', mail_niveau = '', mailPro_niveau = '' WHERE utilisateur.id = $id;" ;
+                        $confidentialite_Ajout = mysql_query($confidentialite_req) ;
+                        }           
+                    
+                } else {
+                    $message_ajout = "<p class=\"erreur\">Erreur lors de l'enregistrement.</p>" ;
+                }
+            } elseif ($mail_ok == false) {
+                $message_ajout = "<p class=\"erreur\">L'adresse E-Mail à ajouter n'a pas le bon format.</p>";
+            }
+        }
 
-	}
+    }
     
     debuthtml ("Annuaire M2 DEFI - Inscription","Annuaire M2 DEFI","Inscription à l'annuaire");
     echo $message_ajout ;
@@ -118,25 +106,25 @@
                         <label for="nom">Nom * : </label>
                         <input type="text" id="nom" name="nom" />
                          <select name=\"affichage_nom\">
-			<option value=\"1\">Affichage privé</option>
-			<option value=\"2\">Affichage public</option>
-			</select>
+            <option value=\"1\">Affichage privé</option>
+            <option value=\"2\">Affichage public</option>
+            </select>
                     </p>
                     <p>
                         <label for="nomPatro">Nom patronymique (nom au moment de votre obtention de diplôme M2 DEFI) : </label>
                         <input type="text" id="nomPatro" name="nomPatro" />
                          <select name=\"affichage_nomPatro\">
-			<option value=\"1\">Affichage privé</option>
-			<option value=\"2\">Affichage public</option>
-			</select>
+            <option value=\"1\">Affichage privé</option>
+            <option value=\"2\">Affichage public</option>
+            </select>
                     </p>
                     <p>
                         <label for="prenom">Prénom * : </label>
                         <input type="text" name="prenom" id="prenom" />
                         <select name=\"affichage_prenom\">
-			<option value=\"1\">Affichage privé</option>
-			<option value=\"2\">Affichage public</option>
-			</select>
+            <option value=\"1\">Affichage privé</option>
+            <option value=\"2\">Affichage public</option>
+            </select>
                     </p>
                     <p>
                         <label for="naissance">Date de naissance * : </label>
@@ -151,9 +139,9 @@
                         <label for="mail">Adresse E-Mail * : </label>
                         <input type="text" id="mail" name="mail" />
                          <select name=\"affichage_mail\">
-			<option value=\"1\">Affichage privé</option>
-			<option value=\"2\">Affichage public</option>
-			</select>
+            <option value=\"1\">Affichage privé</option>
+            <option value=\"2\">Affichage public</option>
+            </select>
                     </p>
                     <p>
                         <label for="mdp">Mot de passe * : </label>
