373f0ca07b6bbe7030c36c56a1aca6fd1ef09cbc
anita test <anita.mazur1@gmail.com>
2012-06-18 16:27:28 +0200

    suppression de la fonction debutmenu

--- php/connexion.php
+++ php/connexion.php
@@ -4,46 +4,39 @@ $connexion = connexion() ;
 
 debuthtml("Annuaire M2 DEFI - Page de connexion","Annuaire M2 DEFI", "Connexion au site","") ;
 ?>
-			<form id="form1" action="pageAccueil.php" method="post">
-				<fieldset>
-					<legend>Si vous avez déjà un compte, veuillez introduire les informations suivantes :</legend>
-					<p>
-						<label for="nom">Nom : </label>
-						<input type="text" id="nom" name="nom" />
-					</p>
-					<p>
-						<label for="prenom">Prénom : </label>
-						<input type="text" id="prenom" name="prenom" />
-					</p>
-					<p>
-						<label for="naissance">Date de naissance : </label>
-						<input type="text" id="naissance" name="naissance" />
-						<label for="format_naissance">(format : yyyy-mm-dd)</label>
-					</p>
-					<p>
-						<label for="mail">Email : </label>
-						<input type="text" id="mail" name="mail" />
-					</p>
-					<p>
-						<label for="mdp">Mot de passe : </label>
-						<input type="password" name="mdp" id="mdp" />
-					</p>
-					<p class="submit">
-						<input type="submit" name="valider" value="Valider" />
-					</p>
-				</fieldset>
-			</form>
-			
-			<?php
-			debutmenu();
-			echo "<li><a href=\"recherche.php\">Recherche</a></li>";
-			finmenu();
-			?>
-			
-			<p>Si vous n'avez pas d'identifiants, veuillez vous inscrire en cliquant sur <a href="inscription.php">Inscription</a></p>
-			<p>Si vous rencontrez des problèmes n'hésitez pas à <a href="mailto:admin@annuairedefi.u-paris10.fr">contacter l'administrateur</a></p>
+            <form id="form1" action="pageAccueil.php" method="post">
+                <fieldset>
+                    <legend>Si vous avez déjà un compte, veuillez introduire les informations suivantes :</legend>
+                    <p>
+                        <label for="nom">Nom : </label>
+                        <input type="text" id="nom" name="nom" />
+                    </p>
+                    <p>
+                        <label for="prenom">Prénom : </label>
+                        <input type="text" id="prenom" name="prenom" />
+                    </p>
+                    <p>
+                        <label for="naissance">Date de naissance : </label>
+                        <input type="text" id="naissance" name="naissance" />
+                        <label for="format_naissance">(format : yyyy-mm-dd)</label>
+                    </p>
+                    <p>
+                        <label for="mail">Email : </label>
+                        <input type="text" id="mail" name="mail" />
+                    </p>
+                    <p>
+                        <label for="mdp">Mot de passe : </label>
+                        <input type="password" name="mdp" id="mdp" />
+                    </p>
+                    <p class="submit">
+                        <input type="submit" name="valider" value="Valider" />
+                    </p>
+                </fieldset>
+            </form>
+            <p>Si vous n'avez pas d'identifiants, veuillez vous inscrire en cliquant sur <a href="inscription.php">Inscription</a></p>
+            <p>Si vous rencontrez des problèmes n'hésitez pas à <a href="mailto:admin@annuairedefi.u-paris10.fr">contacter l'administrateur</a></p>
 
 <?php
 finhtml() ; 
 ?>
-		
+        
