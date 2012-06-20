164f2e85c9200274f1075a243c87f554641d863d
jenni-h <jenni-h@hotmail.fr>
2012-06-20 22:36:01 +0300

    correction des erreurs

--- annuaire_fini/lespromos.php
+++ annuaire_fini/lespromos.php
@@ -12,7 +12,7 @@ $mail = $_SESSION['mail'] ;
 require("fonctions.php") ;
 $connexion = connexion() ;
 
-debuthtml("Annuaire M2 DEFI - les promos","Annuaire M2 DEFI", "Les promotions") ;
+debuthtml("Annuaire M2 DEFI - les promos","Annuaire M2 DEFI", "Les promotions",$id_role) ;
 
 		echo"<table border=\"1px\">
 			<th colspan= 7 >Promotion</th>
@@ -54,6 +54,8 @@ while ($ligne = mysql_fetch_object($res_annee)) {
 				$promo_prenom = $ligne->prenom;
 				$promo_role = $ligne->nom_role;
 				$promo_statut = $ligne->nom_statut;
+				$promo_date_inscription = $ligne->date_inscription;
+				$promo_date_maj_profil = $ligne->date_maj_profil;
 
 				
 						echo "<tr>";
@@ -75,7 +77,7 @@ while ($ligne = mysql_fetch_object($res_annee)) {
 						}
 				else { echo "_</td>";}
 				
-				echo "<td>$promo_role</td>";
+			#	echo "<td>$promo_role</td>";
 
 							
 						## si ancien etudiant ##
@@ -90,17 +92,18 @@ while ($ligne = mysql_fetch_object($res_annee)) {
 								
 								
 								$req_statut2="SELECT *
-								FROM utilisateur AS u, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp
-								WHERE u.id = pu.id_utilisateur
-								AND u.id = eu.id_utilisateur
-								AND p.id = pu.id_poste
-								AND p.id = pde.id_poste
-								AND e.id = eu.id_entreprise
-								AND e.id = pde.id_entreprise
-								AND e.id = ev.id_entreprise
-								AND vi.id = ev.id_entreprise
-								AND vi.id = vp.id_ville
-								AND pa.id = vp.id_pays";
+								FROM utilisateur AS u, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp AND statut_ancien_etudiant AS sa
+									WHERE u.id = pu.id_utilisateur
+									AND u.id = eu.id_utilisateur
+									AND u.id = sa.id_utilisateur
+									AND p.id = pu.id_poste
+									AND p.id = pde.id_poste
+									AND e.id = eu.id_entreprise
+									AND e.id = pde.id_entreprise
+									AND e.id = ev.id_entreprise
+									AND vi.id = ev.id_entreprise
+									AND vi.id = vp.id_ville
+									AND sa.id_statut = '$promo_id_statut' AND ru.id_role ='$promo_id_role'";
 		
 								$res_statut2 = mysql_query($req_statut2) ;
 								
@@ -154,71 +157,69 @@ while ($ligne = mysql_fetch_object($res_annee)) {
 								echo "<td>$promo_statut</td>";
 								
 								$req_statut3 = "SELECT * 
-								FROM utilisateur AS u, etudes AS e, etudes_utilisateur AS eu, etablissement AS eta, etablissement_utilisateur AS etau, ville AS v, pays AS p, ville_pays AS vp, etablissement_ville AS etav
+								FROM utilisateur AS u, etudes AS e, etudes_utilisateur AS eu, etablissement AS eta, etablissement_utilisateur AS etau, ville AS v, pays AS p, ville_pays AS vp, etablissement_ville AS etav, statut_ancien_etudiant AS sa
 								WHERE u.id = eu.id_utilisateur
+								WHERE u.id = sa.id_utilisateur
 								AND u.id = etau.id_utilisateur
 								AND e.id = eu.id_etudes
 								AND eta.id = etau.id_etablissement
 								AND eta.id = etav.id_etablissement
 								AND v.id = vp.id_ville
 								AND v.id = etav.id_ville
-								AND p.id = vp.id_pays" ;
+								AND p.id = vp.id_pays
+								AND sa.id_statut = '$promo_id_statut' AND ru.id_role ='$promo_id_role'" ;
 								
 								$res_statut3 = mysql_query($req_statut3) ;
 								
 									while ($ligne = mysql_fetch_object($res_statut3)) {
-										$diplome = $ligne->diplome;
-											$nom_etab = $ligne->nom_etablissement;
-											$web_etab = $ligne->siteweb_etablissement;
-											$ville = $ligne->nom_ville;
-											$cp = $ligne->cp;
-											$pays = $ligne->nom_pays;
+										$promo_diplome = $ligne->diplome;
+											$promo_nom_etab = $ligne->nom_etablissement;
+											$promo_web_etab = $ligne->siteweb_etablissement;
+											$promo_ville = $ligne->nom_ville;
+											$promo_cp = $ligne->cp;
+											$promo_pays = $ligne->nom_pays;
 									
-							if ($poste!=""){
-								echo "<td>$diplome<br/>";
+							if ($promo_diplome!=""){
+								echo "<td>$promo_diplome<br/>";
 								}
 								else { echo "<td>-<br/>";}
-							if ($nom_etab!=""){
-								echo "$nom_etab<br/>";
+							if ($promo_nom_etab!=""){
+								echo "$promo_nom_etab<br/>";
 								}
 								else { echo "-<br/>";}
-							if ($web_etab!=""){
-								echo "$web_etab<br/>";
+							if ($promo_web_etab!=""){
+								echo "$promo_web_etab<br/>";
 								}
 								else { echo "-<br/>";}
-							if ($ville!=""){
-								echo "$ville<br/>";
+							if ($promo_ville!=""){
+								echo "$promo_ville<br/>";
 								}
 								else { echo "-<br/>";}
-							if ($cp!=""){
-								echo "$cp<br/>";
+							if ($promo_cp!=""){
+								echo "$promo_cp<br/>";
 								}
 								else { echo "- <br/>";}
 								
-							if ($pays!=""){
-								echo "$pays</td>";
+							if ($promo_pays!=""){
+								echo "$promo_pays</td>";
 								}
 								else { echo "-</td>";}	
 									} }
 							
 							
 							##si profil ? remplir ou en recherche d'emploi##
-							elseif ($id_statut==1 or $id_statut ==4)
+							elseif ($promo_id_statut==1 or $promo_id_statut ==4)
 								{
-								echo "<td>$statut</td>";
+								echo "<td>$promo_statut</td>";
 								echo "<td> - </td>";
 								}
 							}
-						echo "<td>$ligne->date_inscription</td>";
-						echo "<td>$ligne->date_maj_profil</td>";
+						echo "<td>$promo_date_inscription</td>";
+						echo "<td>$promo_date_maj_profil</td>";
 						echo "</tr>";
 					 }}
 
 	  echo "</table>";	
-
-	debutmenu();
-	echo "<li><a href=\"pageAccueil.php\">Accueil</a></li>";
-	finmenu();
 	
 	echo "<p>Si vous rencontrez des problémes n'hésitez pas à <a href=\"mailto:admin@annuairedefi.u-paris10.fr\">contacter l'administrateur</a></p>";
 	
