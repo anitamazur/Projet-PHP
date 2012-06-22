17803f8e5bcb74d68ff6bc56e07a6623f8ec2516
jenni-h <jenni-h@hotmail.fr>
2012-06-22 15:13:16 +0300

    suppression AND dans FROm

--- annuaire_fini/export_pdf_tous.php
+++ annuaire_fini/export_pdf_tous.php
@@ -90,7 +90,7 @@ if ($row["id_role"]== 1)
                     {
                     
                     $req_statut2="SELECT *
-				FROM utilisateur AS u, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp AND statut_ancien_etudiant AS sa, statut AS s, roles_utilisateur AS ru, role AS r
+				FROM utilisateur AS u, poste AS p, poste_utilisateur AS pu, poste_dans_entreprise AS pde, entreprise AS e, entreprise_utilisateur As eu, entreprise_ville AS ev, ville AS vi, pays AS pa, ville_pays AS vp ,statut_ancien_etudiant AS sa, statut AS s, roles_utilisateur AS ru, role AS r
 				WHERE u.id = pu.id_utilisateur
 				AND u.id = eu.id_utilisateur
 				AND u.id = sa.id_utilisateur
